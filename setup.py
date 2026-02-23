913ee5d2abfc8c2e9558c283857f2d6a7ccf299c
Tommaso Giacometti <tommaso.giacometti5@unibo.it>
2026-02-23 14:41:39 +0100

    new version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
   license="MIT",
   packages=find_packages(exclude=("tests", "docs")),
   include_package_data=True,
-  install_requires=requirements,
+  #install_requires=requirements,
   python_requires=">=3.8",
   classifiers=[
     "Programming Language :: Python :: 3",
